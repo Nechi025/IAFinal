0f311a2a5dbcfb257beff22e82f24c497ad6f4b5
Nechi025 <ignacioaloswol@hotmail.com>
2026-02-11 14:46:12 -0300

    Seek y Obstacle Avoidance

--- IAFinal/Assets/Scripts/MovementController.cs
+++ IAFinal/Assets/Scripts/MovementController.cs
@@ -4,10 +4,19 @@ public class MovementController : MonoBehaviour
 {
     [Header("Movement")]
     public float maxSpeed = 5f;
-    public float maxForce = 10f;
+    public float maxForce = 8f;
+    public float arriveRadius = 0.5f;
+
+    [Header("Obstacle Avoidance")]
+    public float avoidDistance = 2f;
+    public float avoidForce = 15f;
+    public LayerMask obstacleMask;
 
     private Vector3 velocity;
     private Rigidbody rb;
+
+    [Header("Target")]
+    public Transform target;
 
     void Awake()
     {
@@ -16,19 +25,37 @@ public class MovementController : MonoBehaviour
 
     void Update()
     {
+        if (target == null)
+            return;
+
         Vector3 steering = Vector3.zero;
 
-        // TEST: movimiento constante hacia adelante
-        steering += transform.forward * maxForce;
+        steering += Seek(target.position);
+        steering += ObstacleAvoidance();
 
         ApplyMovement(steering);
     }
 
+    //Steering Seek
+    Vector3 Seek(Vector3 targetPos)
+    {
+        Vector3 desired = targetPos - rb.position;
+        float distance = desired.magnitude;
+
+        if (distance < arriveRadius)
+            return Vector3.zero;
+
+        desired.Normalize();
+        desired *= maxSpeed;
+
+        Vector3 steering = desired - velocity;
+        return Vector3.ClampMagnitude(steering, maxForce);
+    }
+
     void ApplyMovement(Vector3 steering)
     {
-        steering = Vector3.ClampMagnitude(steering, maxForce);
-
         velocity += steering * Time.deltaTime;
+        velocity.y = 0f;
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
 
         rb.MovePosition(rb.position + velocity * Time.deltaTime);
@@ -38,4 +65,31 @@ public class MovementController : MonoBehaviour
             rb.MoveRotation(Quaternion.LookRotation(velocity));
         }
     }
+
+    Vector3 ObstacleAvoidance()
+    {
+        Vector3 avoidance = Vector3.zero;
+        RaycastHit hit;
+
+        Vector3 origin = rb.position;
+        origin.y = 0.5f;
+
+        if (Physics.Raycast(origin, transform.forward, out hit, avoidDistance, obstacleMask))
+        {
+            //Esto para que no tome el eje Y
+            Vector3 normal = hit.normal;
+            normal.y = 0f;
+            normal.Normalize();
+
+            Vector3 away = Vector3.Reflect(transform.forward, normal);
+            away.y = 0f;
+
+            avoidance += away.normalized * avoidForce;
+        }
+
+        Debug.DrawRay(origin, transform.forward * avoidDistance, Color.red);
+
+        return avoidance;
+    }
+
 }
